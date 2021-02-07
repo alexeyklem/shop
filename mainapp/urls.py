a546528c07b6077f6fd9d59888f68522800d694f
alexeyklem <KlepaN567@gmail.com>
2021-02-07 23:39:15 +0300

    make order and test

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,6 +9,7 @@ from .views import (
     DeleteFormCartView,
     ChangeQTYView,
     CheckoutView,
+    MakeOrderView,
 )
 
 
@@ -21,7 +22,8 @@ urlpatterns = (
     path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart'),
     path('remove-from-cart/<str:ct_model>/<str:slug>/', DeleteFormCartView.as_view(), name='delete_from_cart'),
     path('change-qty/<str:ct_model>/<str:slug>/', ChangeQTYView.as_view(), name='change_qty'),
-    path('checkout/', CheckoutView.as_view(), name='checkout')
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('make-order/', MakeOrderView.as_view(), name='make_order')
 )
 
 
