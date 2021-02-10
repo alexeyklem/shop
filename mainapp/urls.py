0b2550c96c371927bfed9e42b910123ba1302cb6
alexeyklem <KlepaN567@gmail.com>
2021-02-11 00:25:07 +0300

    modal info and registration new customer

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from .views import (
     BaseView,
@@ -11,6 +12,8 @@ from .views import (
     CheckoutView,
     MakeOrderView,
     LoginView,
+    RegistrationView,
+    ProfileView,
 )
 
 
@@ -25,7 +28,12 @@ urlpatterns = (
     path('change-qty/<str:slug>/', ChangeQTYView.as_view(), name='change_qty'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('make-order/', MakeOrderView.as_view(), name='make_order'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+
+
 )
 
 
